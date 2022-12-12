e0a8591ffcd93e218e3d177d39bd42905674eeb9
milletgilmore <milletgilmore@gmail.com>
2022-12-12 09:48:27 -0600

    sort only odd numbers in array

---
+++ sort-odd.js
@@ -0,0 +1,10 @@
+/*
+You will be given an array of numbers. 
+You have to sort the odd numbers in ascending order while leaving the 
+even numbers at their original positions.
+*/
+
+function sortArray(array) {
+    const odd = array.filter((x) => x % 2).sort((a,b) => a - b);
+    return array.map((x) => x % 2 ? odd.shift() : x);
+  }
